c15491f111ce00a948961baf920e29a18441aeba
Felagiaanjeli <90699589+Felagiaanjeli@users.noreply.github.com>
2021-09-17 11:53:35 +0700

    Add files via upload

---
+++ Tugas DPW 1/tugas_11.php
@@ -0,0 +1,30 @@
+<?php
+
+echo " kitty adalah anjing<br>";
+echo "punya ekor sebanyak : 1<br>";
+echo "tidak bisa terbang<br>";
+echo "suaranya:guk guk";
+
+echo "<hr>";
+
+echo "sheep adalah kambing<br>";
+echo "punya kaki sebanyak : 4<br>";
+echo "tidak bisa terbang<br>";
+echo "suaranya : mbeek";
+
+echo "<hr>";
+
+echo "doni adalah monyet<br>";
+echo "punya tangan sebanyak : 2<br>";
+echo "suka makan pisang<br>";
+echo "suaranya: cik cik";
+
+echo "<hr>";
+
+echo "bobi adalah buaya<br>";
+echo "punya ekor: 1<br>";
+echo "merupakan hewan buas<br>";
+echo "suaranya : aukkk";
+
+echo "<hr>";
+?>
